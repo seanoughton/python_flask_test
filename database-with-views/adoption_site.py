1990d587a5c842c528944fa670497f736e45a93b
seanoughton <seanoughton@gmail.com>
2018-12-20 12:28:18 -0800

    setting up the application

---
+++ database-with-views/adoption_site.py
@@ -0,0 +1,16 @@
+import os
+# from forms import AddForm,DelForm
+from flask import Flask,render_template,url_for,redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+****************** SQL Database *********************
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
