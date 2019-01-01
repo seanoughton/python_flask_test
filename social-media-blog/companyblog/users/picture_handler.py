9215731e1943f468315ebb973cd88f31b756b88c
seanoughton <seanoughton@gmail.com>
2019-01-01 15:26:51 -0800

    added functionality to creat a blog post and edit it

--- social-media-blog/companyblog/users/picture_handler.py
+++ social-media-blog/companyblog/users/picture_handler.py
@@ -8,11 +8,11 @@ def add_profile_pic(pic_upload,username):
     #this lets you know what the file extension is jpg, png or something else
     ext_type = filename.split('.')[-1]
     #this saves the image as the username.jpg
-    storage_filename  = str(usrname)+'.'+ext_type
+    storage_filename  = str(username)+'.'+ext_type
 
     #get the root path of the app,
     #then look for static profile pics
-    filepath = os.path.join(current_app.root_path,'static\profile_pics',storage_filename)
+    filepath = os.path.join(current_app.root_path,'static/profile_pics',storage_filename)
 
     output_size = (200,200)
     pic = Image.open(pic_upload)
--- social-media-blog/companyblog/users/views.py
+++ social-media-blog/companyblog/users/views.py
@@ -54,13 +54,13 @@ def account():
     if form.validate_on_submit():
         if form.picture.data: #if they actually uploaded some picture data
             username = current_user.username
-            pic = add_profile_pic(form.picture.data.username)
+            pic = add_profile_pic(form.picture.data,username)
             #form.picture.data gets the actual image file
             current_user.profile_image = pic
 
         current_user.username = form.username.data
         current_user.email = form.email.data
-        db.session.commit
+        db.session.commit()
         # flash('User Account Updated')
         return redirect(url_for('users.account'))
 
